be1fd5aee8be00fad50c895bdab1dc56707e21e0
Sunnia <q0423546006@gmail.com>
2012-11-05 14:04:49 +0800

    Revert "Revert "first commit""
    This reverts commit 80305b2b92701109cadb17a8d86852d2ba3dda0e.

---
+++ SunniaWebsocketPrc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SunniaWebsocketPrc
+//
+//  Created by Shih Sunnia on 12/11/5.
+//  Copyright (c) 2012年 Shih Sunnia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
